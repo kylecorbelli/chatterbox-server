bd2dff7e4fa6633c7930af04e0d195839433c15f
pacificLiving <kyle.corbelli@gmail.com>
2016-02-02 11:39:21 -0800

    Refactore moARRR

--- server/request-handler.js
+++ server/request-handler.js
@@ -20,29 +20,14 @@ var Message = function(username, message, roomname) {
   this.username = username;
   this.message = message;
   this.roomname = roomname;
-  // this.timeStamp = new Drate();
 };
 
-// results.push(new Message('TestDude', 'sample', 'cabinet'));
-
-
 var requestHandler = function(request, response) {
-  // Request and Response come from node's http module.
-  //
-  // They include information about both the incoming request, such as
-  // headers and URL, and about the outgoing response, such as its status
-  // and content.
-  //
-  // Documentation for both request and response can be found in the HTTP section at
-  // http://nodejs.org/documentation/api/
 
-  // Do some basic logging.
-  //
-  // Adding more logging to your server can be an easy way to get passive
-  // debugging help, but you should always be careful about leaving stray
-  // console.logs in your code.
+  var url = request.url;
+  var method = request.method;
 
-  console.log("Serving request type " + request.method + " for url " + request.url);
+  console.log("Serving request type " + method + " for url " + url);
 
   // The outgoing status.
   var statusCode = 200;
@@ -54,107 +39,48 @@ var requestHandler = function(request, response) {
     response.end(JSON.stringify({results: results}));
   }
 
-  if (request.method === 'OPTIONS') {
-    writer(statusCode)
-  } else if (request.method === 'GET') {
-    if (request.url === '/log') {
-      writer()
-    } else if (request.url === '/classes/messages') {
-      // do stuff... return all previous messages
-      writer(statusCode)
-    } else if (request.url === '/classes/room1') {
-      // console.log('just got a request for /classes/room1');
+  var bodyParser = function(req) {
+    var body = '', parsedBody, data;
+    req.on('data', function(chunk) {
+      body += chunk.toString();
+    });
+    req.on('end', function() {
+      parsedBody = qs.parse(body);
+      data = JSON.parse(Object.keys(parsedBody)[0]);
+      results.push(new Message(data.username, data.message, data.roomname));
+      writer();
+    });
+  };
+
+  if (method === 'OPTIONS') {
+    writer();
+  } else if (method === 'GET') {
+    if (url === '/log'
+    || url === '/classes/messages'
+    || url === '/classes/room1') {
+      writer();
+    } else {
+      statusCode = 404;
       writer();
     }
-    else {
-      // console.log('=================================GOT HERE===================');
-      statusCode = 404;
-      writer()
-    }
 
-  } else if (request.method === 'POST'){
+  } else if (method === 'POST'){
     statusCode = 201;
-    var body = '', parsedBody, data;
-
-      if ( request.url === '/classes/messages' ) {
-        request.on('data', function(chunk) {
-          body += chunk.toString();
-        });
-        request.on('end', function() {
-          parsedBody = qs.parse(body);
-          data = JSON.parse(Object.keys(parsedBody)[0]);
-          results.push(new Message(data.username, data.message, data.roomname));
-          writer();
-        });
-
-      } else if (request.url === '/classes/room1') {
-        request.on('data', function(chunk) {
-          body += chunk.toString();
-        });
-        request.on('end', function() {
-          parsedBody = qs.parse(body);
-          data = JSON.parse(Object.keys(parsedBody)[0]);
-          results.push(new Message(data.username, data.message, data.roomname));
-          writer();
-        });
-      } else if (request.url === '/send') {
-        // do this
-        request.on('data', function(chunk) {
-          body += chunk.toString();
-        });
-        request.on('end', function() {
-          parsedBody = qs.parse(body);
-          data = JSON.parse(Object.keys(parsedBody)[0]);
-          results.push(new Message(data.username, data.message, data.roomname));
-          writer();
-        });
-
-      }
-      else {
-        statusCode = 404;
-        response.writeHead(statusCode, headers);
-        response.end();
-      }
-      // console.log('POST');
-      // console.log('URL ', request.url);
-    // put results array
-      //
+    if ( url === '/classes/messages'
+      || url === '/classes/room1'
+      || url === '/send') {
+      bodyParser(request);
+    } else {
+      statusCode = 404;
+      response.writeHead(statusCode, headers);
+      response.end();
+    }
   }
 
-  // See the note below about CORS headers.
-
-
-  // Tell the client we are sending them plain text.
-  //
-  // You will need to change this if you are sending something
-  // other than plain text, like JSON or HTML.
-
-
-  // .writeHead() writes to the request line and headers of the response,
-  // which includes the status and all headers.
-  response.writeHead(statusCode, headers);
-
-  // Make sure to always call response.end() - Node may not send
-  // anything back to the client until you do. The string you pass to
-  // response.end() will be the body of the response - i.e. what shows
-  // up in the browser.
-  //
-  // Calling .end "flushes" the response's internal buffer, forcing
-  // node to actually send all the data over to the client.
-  response.end(JSON.stringify({results: results}));
 };
 
 module.exports.requestHandler = requestHandler;
 
-// These headers will allow Cross-Origin Resource Sharing (CORS).
-// This code allows this server to talk to websites that
-// are on different domains, for instance, your chat client.
-//
-// Your chat client is running from a url like file://your/chat/client/index.html,
-// which is considered a different domain.
-//
-// Another way to get around this restriction is to serve you chat
-// client from this domain by setting up static file serving.
 var defaultCorsHeaders = {
   "access-control-allow-origin": "*",
   "access-control-allow-methods": "GET, POST, PUT, DELETE, OPTIONS",
